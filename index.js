9c7f8cd7308e19e1099f2f1b578128b6676b8e18
Pat <patbelcher@yahoo.com>
2023-06-13 16:47:06 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2";
+import { dbCredentials } from "./secrets.js";
+
+const db = mysql.createConnection(dbCredentials);
+
+const query = "SELECT * FROM movies";
+
+db.query(
+    query,
+    function(err, results) {
+        console.log(err);
+        console.table(results);
+
+        for(let i=0; i < results.length; i++) {
+            console.log(results[i]);
+        }
+    }
+)
---
+++ secrets.js
@@ -0,0 +1,8 @@
+export const dbCredentials = {
+    host: '127.0.0.1',
+    database: 'c11mysql',
+    user: 'root',
+    password: 'newpass',
+    port: 3306
+    
+};
